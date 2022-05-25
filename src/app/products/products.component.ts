d1ff889760886eda6cb7d2f4f33a68bcc9748c8e
caglarcercinli <chaglar.ronin@gmail.com>
2022-05-25 20:32:42 +0200

    3 - addedProduct created

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,6 +11,7 @@ export class ProductsComponent implements OnInit {
 
   products: Product[] = [];
   selectedProduct?: Product;
+  addedProduct?: Product;
 
   constructor(private productService: ProductService) {}
 
@@ -23,5 +24,8 @@ export class ProductsComponent implements OnInit {
    onSelect(product: Product): void {
       this.selectedProduct = product;
    }
-
+   onAdd(product:Product): void {
+      console.log(product.id);
+      this.addedProduct = product;
+   }
 }
