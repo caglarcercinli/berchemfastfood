d64d7838ae7cceb3e44bd4b73013c7f043fb9c04
caglarcercinli <chaglar.ronin@gmail.com>
2022-06-20 22:19:11 +0200

    3 - postProduct works

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/service/cart.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 
 @Component({
@@ -11,7 +13,7 @@ export class CartComponent implements OnInit {
 
    public products : any = [];
    public grandTotal !: number;
-   constructor(private cartService : CartService) { }
+   constructor(private cartService : CartService, private httpClient: HttpClient) { }
 
    ngOnInit(): void {
       this.cartService.getProducts()
@@ -23,8 +25,21 @@ export class CartComponent implements OnInit {
     removeItem(item: any){
       this.cartService.removeCartItem(item);
     }
+
     emptycart(){
       this.cartService.removeAllCart();
     }
+
+    orderProduct(): void {
+    const body = JSON.stringify({'user_id': 1});
+    this.httpClient.post('http://localhost:8080/orders', body,
+     {headers: new HttpHeaders({'Content-Type': 'application/json'})
+       })
+          .subscribe(data => {
+            console.log(data);
+       })
+     this.cartService.removeAllCart();
+    }
+
 
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -15,4 +15,5 @@ export class ApiService {
         return res;
       }))
   }
+
 }
